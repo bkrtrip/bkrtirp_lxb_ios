bbdae3ca349f4f58b1b0853016eb4c99d11ee008
bkrtrip <lijianhong@baikeyiyou.com>
2015-05-30 17:07:00 +0800

    adding tab bar

---
+++ LXBtrip/MainTabBar/TabBar.h
@@ -0,0 +1,13 @@
+//
+//  TabBar.h
+//  LXBtrip
+//
+//  Created by Yang Xiaozhu on 15/5/30.
+//  Copyright (c) 2015年 LXB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBar : UITabBar
+
+@end
